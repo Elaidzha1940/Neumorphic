b9e42462a9b194a23c0248b1818d993d59d4f650
Elaidzha1940 <eli080293@gmail.com>
2023-06-03 18:28:53 +0700

    Neumorphic

---
+++ Neumorphic/NeumorphicApp.swift
@@ -0,0 +1,17 @@
+//
+//  NeumorphicApp.swift
+//  Neumorphic
+//
+//  Created by Elaidzha Shchukin on 03.06.2023.
+//
+
+import SwiftUI
+
+@main
+struct NeumorphicApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
